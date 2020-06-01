14d46cc9a7f83d3b89a73b19ce96fec6de3acfe1
Mayur Moundekar <46356484+mayurmoundekar5@users.noreply.github.com>
2020-06-01 21:03:16 +0530

    Update faceRecognition.py

--- faceRecognition.py
+++ faceRecognition.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def faceDetection(test_img):
     gray_img=cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)
-    face_haar_cascade=cv2.CascadeClassifier('C:/Users/mayur/Data Science/Computer Vision/Computer-Vision-with-Python/DATA/haarcascades/haarcascade_frontalface_default.xml')
+    face_haar_cascade=cv2.CascadeClassifier('Computer-Vision-with-Python/DATA/haarcascades/haarcascade_frontalface_default.xml')
     faces=face_haar_cascade.detectMultiScale(gray_img,scaleFactor=1.32,minNeighbors=5)
     
     return faces,gray_img
